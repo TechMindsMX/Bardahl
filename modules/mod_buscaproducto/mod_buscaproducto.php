56b0f471a54f0cb31f1603211753f613cec2f31e
Nestor Ismael Aguilar Estrada <nestor.aguilar@trama.mx>
2014-11-12 13:59:29 -0600

    Modificacion en menu, submenu y modulo busca tu aceite

--- modules/mod_buscaproducto/mod_buscaproducto.php
+++ modules/mod_buscaproducto/mod_buscaproducto.php
@@ -1,53 +1,1 @@
-<?php
-/**
- * @package     Joomla.Site
- * @subpackage  mod_breadcrumbs
- *
- * @copyright   Copyright (C) 2005 - 2014 Open Source Matters, Inc. All rights reserved.
- * @license     GNU General Public License version 2 or later; see LICENSE.txt
- */
-
-defined('_JEXEC') or die;
-
-// Include the syndicate functions only once
-require_once __DIR__ . '/helper.php';
-
-    $result=ModBuscaProductoHelper::getList();
-
-require JModuleHelper::getLayoutPath('mod_buscaproducto', $params->get('layout', 'default'));
-
-
-
-
-
-
-
-
-
-
-
-/*$frm = new html_form();
- 
-$formulario= $frm->openform("form1","post","index.php?option=com_busqueda","multipart/form-data");
-
-$formulario .= $frm->openfieldset("<div id='cabecera'><br/>Encuentra tu producto</div>",550);
-
- 
-
-$formulario .= $frm->addInput("text","producto","Tipo de Producto: ")."<br>";
-$formulario .= $frm->addInput("text","marca","Marca: ")."<br>";
-$formulario .= $frm->addInput("text","modelo","Modelo: ")."<br>";
-$formulario .= $frm->addInput("text","kilometraje","Kilometraje:")."<br>";
-
-$formulario .= $frm->closefieldset();
-
- 
-$formulario .= $frm->addInput("submit","enviar","","Buscar");
-$formulario .= $frm->addInput("reset","reset","","Reset");
-
-$formulario .= $frm->closeform();
-
-
-
-require JModuleHelper::getLayoutPath('mod_buscaproducto', $params->get('layout', 'default'));*/
- 
+<?php/** * @package     Joomla.Site * @subpackage  mod_breadcrumbs * * @copyright   Copyright (C) 2005 - 2014 Open Source Matters, Inc. All rights reserved. * @license     GNU General Public License version 2 or later; see LICENSE.txt */defined('_JEXEC') or die;// Include the syndicate functions only oncerequire_once __DIR__ . '/helper.php';    $result=ModBuscaProductoHelper::getList();require JModuleHelper::getLayoutPath('mod_buscaproducto', $params->get('layout', 'default')); 
--- modules/mod_buscaproducto/tmpl/default.php
+++ modules/mod_buscaproducto/tmpl/default.php
@@ -1,47 +1,1 @@
-<?php
-/**
- * @package     Joomla.Site
- * @subpackage  mod_breadcrumbs
- *
- * @copyright   Copyright (C) 2005 - 2014 Open Source Matters, Inc. All rights reserved.
- * @license     GNU General Public License version 2 or later; see LICENSE.txt
- */
-
-defined ('_JEXEC') or die;
-
-JHtml::_ ('bootstrap.tooltip');
-$jinput = JFactory::getApplication ()->input;
-
-?>
-<form id='form_buscar' name='form1' action='index.php?option=com_busqueda' method='post' enctype='multipart/form-data'>
-	<fieldset id='fieldset'>
-		<label for="select_producto">
-			Tipo de Producto
-		</label><br>
-		<select id="select_producto" name="producto">
-			<option selected>
-			</option>
-			<?php
-			for ($i = 0; $i < count ($result); $i++) {
-				print_r ('<option value="' . $result[$i]->id . '">' . $result[$i]->title . '</option>');
-
-			}?>
-		</select>
-		<br>
-		<label for="marca">
-			Marca:
-		</label><br>
-		<input type='text' name='marca' value='' id='marca'/><br>
-		<label for="modelo">
-			Modelo:
-		</label><br>
-		<input type='text' name='modelo' value='' id='modelo'/><br>
-		<label for="kilometraje">
-			Kilometraje:
-		</label><br>
-		<input type='text' name='kilometraje' value='' id='kilometraje'/><br>
-	</fieldset>
-	<p id="encuentra-producto">
-		<input type='submit' name='enviar' value='Encuentra mi producto'/>
-	</p>
-</form>
+<?php/** * @package     Joomla.Site * @subpackage  mod_breadcrumbs * * @copyright   Copyright (C) 2005 - 2014 Open Source Matters, Inc. All rights reserved. * @license     GNU General Public License version 2 or later; see LICENSE.txt */defined ('_JEXEC') or die;JHtml::_ ('bootstrap.tooltip');$jinput = JFactory::getApplication ()->input;?><form id='form_buscar' name='form1' action='index.php?option=com_busqueda' method='post' enctype='multipart/form-data'      xmlns="http://www.w3.org/1999/html">		<label for="marca">			Marca:		</label><br>		<input type='text' name='marca' value='' id='marca'/><br>		<label for="modelo">			Modelo:		</label><br>		<input type='text' name='modelo' value='' id='modelo'/><br>        <label for="year">            Año:        </label><br>        <input type='text' name='year' value='' id='year'/><br>    <label for="kilometraje">			Kilometraje:		</label><br>		<input type='text' name='kilometraje' value='' id='kilometraje'/><br>	</fieldset>	<p id="encuentra-producto" align="right">		<input id="buscar" type='image' name='enviar'align="right" src="templates/t3_bs3_blank/images/site/btn_buscar.png" />	</p>    <img id="auto" src="templates/t3_bs3_blank/images/site/mustang.png"></form>
